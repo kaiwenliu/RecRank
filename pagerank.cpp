9abd85a0206804f5aa1512cba5d6db1127d20d0e
Thummalapally, Sahith Reddy <sahitht2@illinois.edu>
2022-04-13 22:12:19 -0500

    Started on PageRank implementation

--- pagerank.cpp
+++ pagerank.cpp
@@ -3,8 +3,25 @@
 #include <iostream>
 
 
-PageRank::PageRank(const std::vector<std::pair<int, int>> edges) {
+PageRank::PageRank(const std::vector<std::pair<int, int> > edges) {
     // TODO: create the pagerank and run the algorithm
+    std::pair<int, int> p;
+    p.first = edges.size();
+    p.second = edges.size();
+    std::vector<std::pair<int, int> > ans(1 / (double) edges.size(), p);
+    for(size_t i=0; i< edges.size(); i++){
+        int temp = 0;
+        temp = p.first;
+        p.first = p.second;
+        p.second = temp;
+
+        std::vector<std::pair<int, int> > ans2(1/ (double) edges.size(), p);
+       
+    }
+    
+    
+    
+
 }
 
 vector<PageRankResult> PageRank::result() const {
